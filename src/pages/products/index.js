7c2a36e923d25d82f3ed91222b3798171d29365f
FabioDiCeglie <dcfabio96@gmail.com>
2022-04-06 16:53:07 +0200

    Basic MUI style

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -3,11 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addToBasket, removeFromBasket } from "../../store/basket/actions";
 import { fetchProducts } from "../../store/products/actions";
 import { selectProducts } from "../../store/products/selectors";
-import {
-  selectItems,
-  selectTotal,
-  selectNumberOfItems,
-} from "../../store/basket/selectors";
+import { selectTotal, selectNumberOfItems } from "../../store/basket/selectors";
+import { Container, Grid, AppBar, Button } from "@mui/material";
 
 export default () => {
   const dispatch = useDispatch();
@@ -25,24 +22,50 @@ export default () => {
   }
 
   return (
-    <div>
-      <div>
-        <p>Total Price: {totalPrice}£</p>
-        <p>Number of Items: {numberOfItems}</p>
-      </div>
-      {products?.map((product) => (
-        <div key={product.sku}>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
-          <p>{product.description}</p>
-          <button onClick={() => dispatch(addToBasket(product.sku))}>
-            Add to Basket
-          </button>
-          <button onClick={() => dispatch(removeFromBasket(product.sku))}>
-            Remove to Basket
-          </button>
-        </div>
-      ))}
-    </div>
+    <Container>
+      <AppBar>
+        <span>Total Price: £{totalPrice}</span>
+        <span>Number of Items: {numberOfItems}</span>
+      </AppBar>
+      <br />
+      <br />
+      <br />
+      <Grid container>
+        {products?.map((product) => (
+          <Grid item key={product.sku}>
+            <Grid container spacing={2}>
+              <Grid item xs>
+                {product.name}
+              </Grid>
+              <Grid item xs>
+                {product.price}
+              </Grid>
+              <Grid item xs>
+                {product.description}
+              </Grid>
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  onClick={() => dispatch(addToBasket(product.sku))}
+                >
+                  Add to Basket
+                </Button>
+              </Grid>
+              <Grid item xs>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  color="error"
+                  onClick={() => dispatch(removeFromBasket(product.sku))}
+                >
+                  Remove to Basket
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
